067254983107033501aaaea7b97a53ad0e544c24
mwz73trz <michaelzura@bellsouth.net>
2022-02-04 12:40:49 -0500

    initial backend commit

---
+++ backend/champion_app/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+class SuperBowl(models.Model):
+    name = models.CharField(max_length=25)
+    date_played = models.DateField()
+    win_team = models.CharField(max_length=75)
+    win_score = models.IntegerField()
+    lose_team = models.CharField(max_length=75)
+    stadium = models.CharField(max_length=75)
+    where = models.CharField(max_length=75)
+    mvp = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return f"{self.name}"
+    
+class WorldSeries(models.Model):
+    year_played = models.IntegerField()
+    win_team = models.CharField(max_length=75)
+    winner_wins = models.IntegerField()
+    lose_team = models.CharField(max_length=75)
+    loser_wins = models.IntegerField()
+    
+    def __str__(self):
+        return f"{self.year_played}"
